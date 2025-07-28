55a6b2902ebb561476d476be82001022348c1f1b
Vincent Cricelli <gnosticrecovery@gmail.com>
2025-07-28 17:22:06 -0400

    Update astro_layer.py

--- bluebird_sdk/astro_layer.py
+++ bluebird_sdk/astro_layer.py
@@ -1,1 +1,23 @@
-# Astro Layer
+# Astro Layer - basic symbolic hooks for planetary archetypes
+
+from typing import List
+
+PLANET_ARCHETYPES = {
+    "Saturn": ["Shadow", "Judgment", "Discipline"],
+    "Moon": ["Emotion", "Cycle", "Uncertainty"],
+    "Mercury": ["Mind", "Communication", "Duality"],
+    "Venus": ["Love", "Attraction", "Harmony"],
+    "Mars": ["Drive", "Anger", "Courage"]
+}
+
+class AstroLayer:
+    def __init__(self, active_transits: List[str]):
+        self.transits = active_transits
+
+    def modulate_archetypes(self, active_archetypes: List[str]) -> List[str]:
+        modulated = list(active_archetypes)
+        for planet in self.transits:
+            if planet in PLANET_ARCHETYPES:
+                modulated.extend(PLANET_ARCHETYPES[planet])
+        return list(set(modulated))
+
