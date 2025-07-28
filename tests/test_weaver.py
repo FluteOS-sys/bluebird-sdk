01ea8af868fc51278067b1aef6dba5f40575cf45
Vincent Cricelli <gnosticrecovery@gmail.com>
2025-07-28 17:30:49 -0400

    Update test_weaver.py

--- tests/test_weaver.py
+++ tests/test_weaver.py
@@ -1,1 +1,10 @@
-# Test Weaver
+from bluebird_sdk.weaver import Weaver
+
+def test_parse_and_update():
+    w = Weaver()
+    test_input = "There’s fog again. I can’t see what I’m becoming."
+    neuron = w.parse_entry(test_input)
+    assert "trigger" in neuron
+    assert isinstance(neuron["emotion_profile"], dict)
+    assert "resonance_weight" in neuron
+
