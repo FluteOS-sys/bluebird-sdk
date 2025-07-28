4886fc065cd479d679c4a2f9ed8e620bc1d204d3
Vincent Cricelli <gnosticrecovery@gmail.com>
2025-07-28 17:21:42 -0400

    Update __init__.py
    full script

--- bluebird_sdk/__init__.py
+++ bluebird_sdk/__init__.py
@@ -1,1 +1,12 @@
-#   Init  
+"""
+Bluebird SDK
+Symbolic memory system for emotionally intelligent agents.
+"""
+
+from .weaver import Weaver
+from .lattice import SymbolicLattice
+from .resonance import ResonanceEngine
+from .astro_layer import AstroLayer
+from .mirror_prompt import generate_mirror_prompt
+from .config import DEFAULT_NEURON_TEMPLATE
+  
