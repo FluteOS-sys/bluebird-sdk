93861a43729da181d718b30b04a643ff542c1602
Vincent Cricelli <gnosticrecovery@gmail.com>
2025-07-28 17:22:33 -0400

    Update config.py

--- bluebird_sdk/config.py
+++ bluebird_sdk/config.py
@@ -1,1 +1,10 @@
-# Config
+# Default configuration for symbolic neurons
+
+DEFAULT_NEURON_TEMPLATE = {
+    "emotion_profile": {},
+    "meaning_seed": "",
+    "archetypal_refs": [],
+    "resonance_weight": 0.5,
+    "timestamp": None
+}
+
